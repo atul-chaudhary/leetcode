0ba4c3817da9155e59a5aa558d986cf80bcb94a3
Atul Chaudhary <chaudharyatul444@gmail.com>
2022-05-11 11:50:01 +0530

    Simple Merge two sorted array variation

--- src/com/atul/arrays/MergeSortedArray.java
+++ src/com/atul/arrays/MergeSortedArray.java
@@ -4,25 +4,50 @@ import java.util.Arrays;
 
 public class MergeSortedArray {
     public static void main(String[] args) {
-        int[]  nums1 = {1,2,3,0,0,0};
-        int m = 3;
-        int[] nums2 = {2,5,6};
-        int n = 3;;
-        merge(nums1, m, nums2, n);
-        System.out.println(Arrays.toString(nums1));
+//        int[]  nums1 = {1,2,3,0,0,0};
+//        int m = 3;
+//        int[] nums2 = {2,5,6};
+//        int n = 3;;
+//        merge(nums1, m, nums2, n);
+//        System.out.println(Arrays.toString(nums1));
+
+        int[] nums1 = {1, 5, 9, 10, 15, 20};
+        int m = nums1.length;
+        int[] nums2 = {2, 3, 8, 13};
+        int n = nums2.length;
+        mergeVariation(nums1, m, nums2, n);
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int p1 = m - 1;
         int p2 = n - 1;
-        int k = m+n-1;
+        int k = m + n - 1;
         while (p2 >= 0) {
-            if(p1>=0 && nums1[p1] > nums2[p2]){
+            if (p1 >= 0 && nums1[p1] > nums2[p2]) {
                 nums1[k--] = nums1[p1--];
-            }else {
-                nums1[k--]= nums2[p2--];
+            } else {
+                nums1[k--] = nums2[p2--];
             }
         }
-    //    System.out.println(Arrays.toString(nums1));
+    }
+
+    //one new variation
+    public static void mergeVariation(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = 0, p2 = 0, k = 0;
+        int[] arr = new int[m + n];
+        for (int i = 0; i < arr.length; i++) {
+            if (p1 < m && p2 < n) {
+                if (nums1[p1] < nums2[p2]) {
+                    arr[i] = nums1[p1++];
+                } else {
+                    arr[i] = nums2[p2++];
+                }
+            } else if (p1 < m) {
+                arr[i] = nums1[p1++];
+            } else {
+                arr[i] = nums2[p2++];
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
