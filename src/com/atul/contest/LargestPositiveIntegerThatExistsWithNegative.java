9b067a8b2e879754dc8203ea376c29485c5342dc
Atul Chaudhary <chaudharyatul444@gmail.com>
2022-10-18 22:14:49 +0530

    leetcode questions

--- src/com/atul/contest/LargestPositiveIntegerThatExistsWithNegative.java
+++ src/com/atul/contest/LargestPositiveIntegerThatExistsWithNegative.java
@@ -1,29 +1,107 @@
 package com.atul.contest;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.TreeMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
 
 public class LargestPositiveIntegerThatExistsWithNegative {
-    public static void main(String[] args) {
-        int[] arr = {-10,8,6,7,-2,-3};
-        System.out.println(findMaxK(arr));
-    }
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
 
-
-    public static int findMaxK(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        int max = Integer.MIN_VALUE;
-        for (int it : nums) {
-            if (it < 0 && set.contains(Math.abs(it))) {
-                max = Math.max(max, Math.abs(it));
-            } else if (it > 0 && set.contains(-it)){
-                max = Math.max(max, it);
-            }
-            set.add(it);
+        public TreeNode(int val) {
+            this.val = val;
         }
 
-        return max == Integer.MIN_VALUE ? -1 : max;
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    '}';
+        }
     }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+
+        root.right = new TreeNode(3);
+        root.right.right = new TreeNode(4);
+        root.right.left = new TreeNode(2);
+        root.right.left.left = new TreeNode(4);
+
+
+
+//        TreeNode root = new TreeNode(0);
+//        root.left = new TreeNode(0);
+//        root.left.left = new TreeNode(0);
+//
+//        root.right = new TreeNode(0);
+//        root.right.right = new TreeNode(0);
+//        root.right.right.right = new TreeNode(0);
+        System.out.println(InorderTrav(root));
+        System.out.println(checker(root));
+
+    }
+    public final static List<TreeNode> checker(TreeNode root) {
+        HashMap<String, Integer> map = new HashMap<>();
+        List<TreeNode> result = new ArrayList<>();
+        Queue<TreeNode> pq = new LinkedList<>();
+        pq.offer(root);
+        while (!pq.isEmpty()) {
+            int size = pq.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = pq.poll();
+                String str = InorderTrav(node);
+                if (map.get(str) != null && map.get(str) == 1) {
+                    result.add(node);
+                }
+
+                map.put(str, map.getOrDefault(str, 0)+1);
+
+                if (node.left != null) {
+                    pq.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    pq.offer(node.right);
+                }
+            }
+        }
+        return result;
+    }
+
+
+
+    private static String InorderTrav(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+
+        String inorder = "";
+        String left = InorderTrav(node.left);
+        if(left == null){
+            inorder+="Lnull";
+        } else{
+            inorder+=left;
+        }
+        inorder += node.val+"-";
+        String right = InorderTrav(node.right);
+        if(right == null){
+            inorder+="Rnull";
+        } else{
+            inorder+=right;
+        }
+        return inorder;
+    }
+
 }
