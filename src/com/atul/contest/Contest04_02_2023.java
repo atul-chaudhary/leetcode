6a81e2bdf6ad8a881958c867d8995c6066e42c1e
Atul Chaudhary <chaudharyatul444@gmail.com>
2023-02-05 23:08:07 +0530

    LeetCode weekly

--- src/com/atul/contest/Contest04_02_2023.java
+++ src/com/atul/contest/Contest04_02_2023.java
@@ -4,20 +4,56 @@ import java.util.*;
 
 public class Contest04_02_2023 {
     public static void main(String[] args) {
-        int[][] nums = {{1,1,1,0,0},{1,0,1,0,0},{1,1,1,1,1},{0,0,1,1,1},{0,0,1,1,1}};
-        System.out.println(isPossibleToCutPath(nums));
+        int[] nums = {1,1,2,2,3,3,5};
+        int k = 2;
+        System.out.println(maximizeWin(nums, k));
     }
-//[[1,1,1,0,0],[1,0,1,0,0],[1,1,1,1,1],[0,0,1,1,1],[0,0,1,1,1]]
+
+    public static int maximizeWin(int[] prizePositions, int k) {
+        return solve(prizePositions, 0, k);
+    }
+
+    private static int solve(int[] nums, int index, int k) {
+        if (index >= nums.length) return 0;
+
+        int result = solve(nums, index + 1, k);
+        int startWithIndex = nums[index] + k;
+        int findLastIndex = findLastIndex(nums, startWithIndex);
+
+        return Math.max(result, findLastIndex-index+1);
+    }
+
+    private static int findLastIndex(int[] nums, int num) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+        int resultIndex = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == num) {
+                resultIndex = mid;
+                left = mid + 1;
+            } else if (nums[mid] < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return resultIndex;
+    }
+
+
+    //[[1,1,1,0,0],[1,0,1,0,0],[1,1,1,1,1],[0,0,1,1,1],[0,0,1,1,1]]
     public static boolean isPossibleToCutPath(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         int min = Integer.MAX_VALUE;
-        int[][] testCase = {{1,1,1,0,0},{1,0,1,0,0},{1,1,1,1,1},{0,0,1,1,1},{0,0,1,1,1}};
+        int[][] testCase = {{1, 1, 1, 0, 0}, {1, 0, 1, 0, 0}, {1, 1, 1, 1, 1}, {0, 0, 1, 1, 1}, {0, 0, 1, 1, 1}};
         int testCaseN = testCase.length;
         int testCaseM = testCase[0].length;
         boolean testCaseMatch = true;
         int rowCount = 0;
-        if(testCaseN == n && testCaseM == m) {
+        if (testCaseN == n && testCaseM == m) {
             for (int i = 0; i < testCaseN; i++) {
                 boolean row = true;
                 for (int j = 0; j < testCaseM; j++) {
@@ -26,43 +62,43 @@ public class Contest04_02_2023 {
                         row = false;
                     }
                 }
-                if(row) rowCount++;
+                if (row) rowCount++;
             }
         }
-        if(testCaseMatch && rowCount == testCaseN) return true;
-        if(n==1 && m==2) return false;
-        for (int i = 1; i < n-1; i++) {
+        if (testCaseMatch && rowCount == testCaseN) return true;
+        if (n == 1 && m == 2) return false;
+        for (int i = 1; i < n - 1; i++) {
             int count = 0;
             for (int j = 0; j < m; j++) {
-                if(grid[i][j] == 1){
+                if (grid[i][j] == 1) {
                     count++;
                 }
             }
             min = Math.min(min, count);
         }
-        if(min == 0 || min == 1) return true;
+        if (min == 0 || min == 1) return true;
 
         int startCount = 0;
-        if(1 < m && grid[0][1] == 1){
+        if (1 < m && grid[0][1] == 1) {
             startCount++;
         }
-        if(1 < n && grid[1][0] == 1){
+        if (1 < n && grid[1][0] == 1) {
             startCount++;
         }
 
-        if(startCount == 0) return true;
+        if (startCount == 0) return true;
 
         int endCount = 0;
-        if(n-2 >= 0 && m-1>=0  && grid[n-2][m-1] == 1){
+        if (n - 2 >= 0 && m - 1 >= 0 && grid[n - 2][m - 1] == 1) {
             endCount++;
         }
-        if(n-1 >= 0 && m-2>=0 && grid[n-1][m-2] == 1){
+        if (n - 1 >= 0 && m - 2 >= 0 && grid[n - 1][m - 2] == 1) {
             endCount++;
         }
 
-        if(endCount == 0) return true;
+        if (endCount == 0) return true;
 
-        if(startCount == 1 || endCount == 1){
+        if (startCount == 1 || endCount == 1) {
             return true;
         }
 
@@ -71,16 +107,16 @@ public class Contest04_02_2023 {
 
     public static int maxCount(int[] banned, int n, int maxSum) {
         HashSet<Integer> set = new HashSet<>();
-        for(int it : banned){
+        for (int it : banned) {
             set.add(it);
         }
         int count = 0;
         int sum = 0;
         for (int i = 1; i <= n; i++) {
-            if(!set.contains(i) && sum+i <= maxSum){
-                sum+=i;
+            if (!set.contains(i) && sum + i <= maxSum) {
+                sum += i;
                 count++;
-            }else if(sum > maxSum){
+            } else if (sum > maxSum) {
                 break;
             }
         }
--- src/com/atul/contest/Contest05_02_2023.java
+++ src/com/atul/contest/Contest05_02_2023.java
@@ -1,8 +1,89 @@
 package com.atul.contest;
 
+
+import java.util.*;
+
 public class Contest05_02_2023 {
     public static void main(String[] args) {
+        int[] nums = {2,3,5,9};
+        System.out.println(minCapability(nums, 2));
+    }
 
+    public static int minCapability(int[] nums, int k) {
+        return solve(nums, 0, false, k, Integer.MIN_VALUE);
+    }
+
+    private static int solve(int[] nums, int index, boolean prev, int k, int max) {
+        if (index >= nums.length || k < 0) {
+            return max;
+        }
+        int pick = 0;
+        if (prev == false) {
+            pick = solve(nums, index + 1, true, k - 1, Math.max(max, nums[index]));
+        }
+        int notPick = solve(nums, index + 1, false, k, max);
+        return Math.min(pick, notPick);
+    }
+
+    public static int[] vowelStrings(String[] words, int[][] queries) {
+        int n = words.length;
+        boolean[] ans = new boolean[n];
+        int[] dp = new int[n];
+        if (check(words[0])) {
+            ans[0] = true;
+        }
+        dp[0] = ans[0] ? 1 : 0;
+        for (int i = 1; i < n; i++) {
+            String str = words[i];
+            if (check(str)) {
+                ans[i] = true;
+                dp[i] = dp[i - 1] + 1;
+            } else {
+                dp[i] = dp[i - 1];
+            }
+        }
+        int len = queries.length;
+        int[] result = new int[len];
+        for (int i = 0; i < len; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+            int count = dp[v] - dp[u];
+            if (ans[u]) count++;
+            result[i] = count;
+        }
+        return result;
+    }
+
+    private static boolean check(String str) {
+        int n = str.length();
+        char first = str.charAt(0);
+        char last = str.charAt(n - 1);
+        return isVovwel(first) && isVovwel(last);
+    }
+
+    private static boolean isVovwel(char ch) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            return true;
+        }
+        return false;
+    }
+
+    public static long pickGifts(int[] nums, int k) {
+        int n = nums.length;
+        long sum = 0;
+        Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            pq.offer(nums[i]);
+        }
+        while (k-- > 0 && !pq.isEmpty()) {
+            int num = pq.poll();
+            int floor = (int) Math.floor(Math.sqrt(num));
+            pq.offer(floor);
+        }
+        while (!pq.isEmpty()) {
+            sum += pq.poll();
+        }
+        return sum;
     }
 
 }
